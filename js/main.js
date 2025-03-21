3d74b6c2707c62d30b015308228426d0771697ce
AdrianTayeh <81419977+AdrianTayeh@users.noreply.github.com>
2025-03-21 10:33:39 +0100

    fixing dark mode

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 const toggleSwitch = document.querySelector('.switch input');
+
 function setTheme(theme) {
+    console.log(`Theme set to: ${theme}`);
     document.documentElement.setAttribute('data-theme', theme);
     toggleSwitch.checked = theme === 'dark';
 }
 
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-setTheme(prefersDarkScheme ? 'dark' : 'light');
+const initialTheme = prefersDarkScheme ? 'dark' : 'light';
+setTheme(initialTheme);
 
 toggleSwitch.addEventListener('change', event => {
     const theme = event.target.checked ? 'dark' : 'light';
-    console.log(theme);
     setTheme(theme);
 });
