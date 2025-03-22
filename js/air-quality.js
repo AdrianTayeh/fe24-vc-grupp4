a716875c16f1354cf6f059242a16684d374f597b
Sparven0 <158517054+Sparven0@users.noreply.github.com>
2025-03-22 11:43:26 +0100

    Pollen detailed information tab added

--- js/air-quality.js
+++ js/air-quality.js
@@ -25,10 +25,34 @@ searchCityForm.addEventListener('submit', async (event) => {
 
 async function displayPollenInfo(cName) {
     const pollenInfoDiv = document.querySelector('.pollen-info');
+    const pollenDetailsDiv = document.querySelector('.pollen-details');
     const capitalizedName = cName.charAt(0).toUpperCase() + cName.slice(1);
     const pollenData = await fetchPollenData(cName);
     console.log(pollenData.data[0]);
     
+
+    const speciesData = pollenData.data[0].Species;
+
+    if (speciesData) {
+        let speciesList = '';
+        for (const [category, data] of Object.entries(speciesData)) {
+            speciesList += `<h3>${category} Pollen:</h3><ul>`;
+            if (typeof data === 'object') {
+                for (const [pollenType, percentage] of Object.entries(data)) {
+                    speciesList += `<li>${pollenType}: ${percentage}%</li>`;
+                }
+            } else {
+
+                speciesList += `<li>${category}: ${data}%</li>`;
+            }
+            speciesList += `</ul>`;
+        }
+        pollenDetailsDiv.innerHTML = `${speciesList}`;
+    }else{
+        console.log('No data found')
+    }
+
+
     const allergens = pollenData.data[0].Risk;
 
     const allergensList = Object.entries(allergens).map(([allergen, level]) => 
