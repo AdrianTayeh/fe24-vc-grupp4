ee5a11ec71c2bc70bcbfdd720d6f18ff835647ce
AdrianTayeh <81419977+AdrianTayeh@users.noreply.github.com>
2025-03-23 15:19:39 +0100

    switch is checked if user has dark mode enabled on load

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const toggleSwitch = document.querySelectorAll('.switch input');
 function setTheme(theme) {
     console.log(`Theme set to: ${theme}`);
     document.documentElement.setAttribute('data-theme', theme);
-    toggleSwitch.checked = theme === 'dark';
+    toggleSwitch.forEach(switchEl => {
+        switchEl.checked = theme === 'dark';
+    })
 }
 
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
