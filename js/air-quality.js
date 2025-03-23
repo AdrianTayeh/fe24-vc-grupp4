7c0b67f92e4c5e170ea30ba67b2e191f737c95ff
Sparven0 <158517054+Sparven0@users.noreply.github.com>
2025-03-23 13:42:43 +0100

    Update air-quality.js

--- js/air-quality.js
+++ js/air-quality.js
@@ -4,7 +4,16 @@ import { getCityName } from "./api-fetches.js";
 import { weatherFetchCurrent } from "./api-fetches.js";
 import { weatherFetchForecast } from "./api-fetches.js";
 
-function intitializePollen(){
+
+const switchPollenAQ = document.querySelector('.switchPollenAQ input');
+let categoryGlob = switchPollenAQ.checked ? 'pollen' : 'air-quality';
+
+switchPollenAQ.addEventListener('change', (event) => {
+    categoryGlob = event.target.checked ? 'pollen' : 'air-quality';
+});
+
+
+function intitializeData(){
     navigator.geolocation.getCurrentPosition(
       async (position) => {
         const { latitude, longitude } = position.coords;
@@ -14,18 +23,16 @@ function intitializePollen(){
         console.log(currentData);
         const forecast = await weatherFetchForecast(cityName[0].name);
         const cityNameIn  = forecast.city.name;
+if(categoryGlob == 'pollen'){
         displayPollenInfo(cityNameIn);
+      }else{
+        displayAirQuality(cityNameIn);
+      }
       return cityName;
       });
   }
-  intitializePollen();
+  intitializeData();
 
-const switchPollenAQ = document.querySelector('.switchPollenAQ input');
-let categoryGlob = switchPollenAQ.checked ? 'pollen' : 'air-quality';
-
-switchPollenAQ.addEventListener('change', (event) => {
-    categoryGlob = event.target.checked ? 'pollen' : 'air-quality';
-});
 
 
 const searchCityForm = document.querySelector('#cities-form');
@@ -84,6 +91,48 @@ async function displayPollenInfo(cName) {
 
 
 
+/*async function displayAirQuality(cName) {
+    const pollenInfoDiv = document.querySelector('.pollen-info');
+    const pollenDetailsDiv = document.querySelector('#pollen-data-sidebar');
+    const capitalizedName = cName.charAt(0).toUpperCase() + cName.slice(1);
+    //const pollenData = await fetchPollenData(cName);
+  
+    
+
+    const speciesData = pollenData.data[0].Species;
+
+    if (speciesData) {
+        let speciesList = '';
+        for (const [category, data] of Object.entries(speciesData)) {
+            speciesList += `<h3>${category} Pollen:</h3><ul>`;
+            if (typeof data === 'object') {
+                for (const [pollenType, percentage] of Object.entries(data)) {
+                    speciesList += `<li>${pollenType}: ${percentage}%</li>`;
+                }
+            } else {
+
+                speciesList += `<li>${category}: ${data}%</li>`;
+            }
+            speciesList += `</ul>`;
+        }
+        pollenDetailsDiv.innerHTML = `${speciesList}`;
+    }else{
+        console.log('No data found')
+    }
+
+
+    const allergens = pollenData.data[0].Risk;
+
+    const allergensList = Object.entries(allergens).map(([allergen, level]) => 
+        `<li>${allergen.replace('_', ' ')}: ${level}</li>`
+    ).join('');
+
+   
+    pollenInfoDiv.innerHTML = `
+    <h2>Pollen data for ${capitalizedName}</h2>
+    <ul>${allergensList}</ul>`;
+}
 
    
 
+*/
